8b1e3159d42a6e570e0182ab389e8ddba14780cb
Lukas Subel <s9lusube@stud.uni-saarland.de>
2016-02-13 23:22:17 +0100

    Added progress bar, added better way to display value,

--- js/HTMLActuator.js
+++ js/HTMLActuator.js
@@ -2,28 +2,52 @@ function HTMLActuator() {
   this.questionLeftContainer      = document.querySelector(".question-left");
   this.questionOperatorContainer  = document.querySelector(".question-operator");
   this.questionRightContainer     = document.querySelector(".question-right");
-  this.scoreContainer   = document.querySelector(".score-container");
-  this.bestContainer    = document.querySelector(".best-container");
-  this.messageContainer = document.querySelector(".game-message");
+  this.scoreContainer             = document.querySelector(".score-container");
+  this.bestContainer              = document.querySelector(".best-container");
+  this.messageContainer           = document.querySelector(".game-message");
+  this.progressbar                = document.querySelector(".progress-bar");
 
-  this.score = 0;
+  this.timeReference  = new Date();
+  this.score          = 0;
+}
+
+HTMLActuator.prototype.calculateStrLength = function (value, base){
+  var length = 1;
+  while(value / base >= 1){
+    value = value / base;
+    length += 1;
+  }
+  return length;
 }
 
 HTMLActuator.prototype.actuate = function (questions, metadata) {
   var self = this;
 
   window.requestAnimationFrame(function () {
+    // reset containers + progressbar
     self.clearContainer(self.questionLeftContainer);
     self.clearContainer(self.questionOperatorContainer);
     self.clearContainer(self.questionRightContainer);
 
-    // write down the current questions
-    self.addQuestions(self.questionLeftContainer,   questions.leftAnswer,   questions.leftBase);
-    self.setOperator(self.questionOperatorContainer, questions.operator);
-    self.addQuestions(self.questionRightContainer,  questions.rightAnswer,  questions.rightBase);
+    var timediff            = Date.now() + questions.answertime;
+    self.progressbar.max    = questions.answertime;
+    self.progressbar.value  = questions.answertime;
+    self.answertime         = timediff;
 
+    var left_length   = self.calculateStrLength(questions.leftAnswer,   questions.leftBase);
+    var right_length  = self.calculateStrLength(questions.rightAnswer,  questions.rightBase);
+    var string_length = Math.max(left_length, right_length);
+
+    // write down the current questions
+    self.addQuestions(self.questionLeftContainer,   questions.leftAnswer,   questions.leftBase, string_length);
+    self.setOperator(self.questionOperatorContainer, questions.operator);
+    self.addQuestions(self.questionRightContainer,  questions.rightAnswer,  questions.rightBase, string_length);
+
+    // update score + bestscore
     self.updateScore(metadata.score);
     self.updateBestScore(metadata.bestScore);
+
+    self.updateProgressBar(questions.answertimeoutcallback);
   });
 };
 
@@ -33,14 +57,27 @@ HTMLActuator.prototype.clearContainer = function (container) {
   }
 };
 
-HTMLActuator.prototype.addQuestions = function(container, value, base){
+HTMLActuator.prototype.addQuestions = function(container, value, base, length){
   var self = this;
 
-  var wrapper   = document.createElement("div");
-  var inner     = document.createElement("div");
+  var wrapper     = document.createElement("div");
+  var inner       = document.createElement("div");
+  var elem_value  = document.createElement("span");
+  var elem_base   = document.createElement("span");
 
   inner.classList.add("tile-inner");
-  inner.textContent = value + "_" + base;
+  elem_value.classList.add("math-value");
+  elem_base.classList.add("math-base");
+
+  var str_value = value.toString(base);
+  while(str_value.length < length){
+    str_value = "0" + str_value;
+  }
+  elem_value.textContent = str_value;
+  elem_base.textContent = base;
+
+  inner.appendChild(elem_value);
+  inner.appendChild(elem_base);
 
   // Add the inner part of the tile to the wrapper
   wrapper.appendChild(inner);
@@ -85,3 +122,18 @@ HTMLActuator.prototype.updateScore = function (score) {
 HTMLActuator.prototype.updateBestScore = function (bestScore) {
   this.bestContainer.textContent = bestScore;
 };
+
+HTMLActuator.prototype.updateProgressBar = function(callback){
+  var self = this;
+  var timediff = self.answertime - Date.now();
+  // if below zero, time has run out
+  if(timediff < 0){
+    callback();
+  }
+  else{
+    self.progressbar.value = timediff;
+    window.requestAnimationFrame(function(){
+      self.updateProgressBar(callback);
+    });
+  }
+}
